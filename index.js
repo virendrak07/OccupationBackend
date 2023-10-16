15c380f06baef8529c166ef039b24a927a221cf5
virendrak07 <virenkadam07@gmail.com>
2023-10-16 18:50:37 +0200

    Update index.js
    stack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const { mogoUrl } = require('./keys')
 
 
