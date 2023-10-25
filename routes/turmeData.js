90bb44f72badf6adf4d476bffe71105c0c161abc
virendrak07 <virenkadam07@gmail.com>
2023-10-25 12:27:58 +0200

    Update turmeData.js
    get api Turme Game

--- routes/turmeData.js
+++ routes/turmeData.js
@@ -14,5 +14,34 @@ router.post('/turmeData', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+/**
+ * This function represents get turmeData Game data by patientId
+ * @param {*} req
+ * @param {*} res
+ * @author Virendra Kadam
+ */
+router.get('/turmeData-game/:patientId', async (req, res) => {
+    try {
+        const { patientId } = req.params;
 
+        const gameData = await TurmeData.find({ patientId });
+
+        const formattedGameData = gameData.map((game) => {
+            const dateObj = new Date(game.date);
+            const formattedDate = dateObj.toLocaleDateString();
+            const formattedTime = dateObj.toLocaleTimeString();
+
+            return {
+                ...game._doc,
+                formattedDate,
+                formattedTime,
+            };
+        });
+
+        res.status(200).json(formattedGameData);
+    } catch (error) {
+        console.error('Error fetching Turme Game data:', error);
+        res.status(500).json({ message: 'An error occurred while fetching the Turme Game data.' });
+    }
+});
 module.exports = router;
