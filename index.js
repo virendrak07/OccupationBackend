e6ab6d089ac3c86ed2196fe730abf56941250063
virendrak07 <virenkadam07@gmail.com>
2024-02-02 11:41:24 +0100

    new version

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 4000;
-const { mogoUrl } = require('./keys')
+const PORT = 3001;
+const { mogoUrl } = require('./keys');
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'exp://192.168.1.7:8081',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+}));
 
 
+app.options('*', cors()); // Enable preflight requests for all routes
+
+app.use(bodyParser.json());
 require('./models/userData');
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+const requireToken = require('./middleware/requireToken');
+const authRoutes = require('./routes/authRoutes');
+const patientRoutes = require('./routes/patientData');
+const zielenGameRoutes = require('./routes/zielenGame');
+const zielenTrainingRoutes = require('./routes/zielenTraining');
+const tippenGameRoutes = require('./routes/tippenData');
+const gewindeDataRoutes = require('./routes/gewindeData');
+const nachfahrenDataRoutes = require('./routes/nachfahrenData');
+const turmeDataRoutes = require('./routes/turmeData');
+const klotzeDataRoutes = require('./routes/klotzeData');
+const umdrehenDataRoutes = require('./routes/umdrehenData');
 
-const requireToken = require('./middleware/requireToken')
-const authRoutes = require('./routes/authRoutes')
-const patientRoutes = require('./routes/patientData')
-const zielenGameRoutes = require('./routes/zielenGame')
-const zielenTrainingRoutes = require('./routes/zielenTraining')
-const tippenGameRoutes = require('./routes/tippenData')
-const gewindeDataRoutes = require('./routes/gewindeData')
-const nachfahrenDataRoutes = require('./routes/nachfahrenData')
-const turmeDataRoutes = require('./routes/turmeData')
-const klotzeDataRoutes = require('./routes/klotzeData')
-const umdrehenDataRoutes = require('./routes/umdrehenData')
-
-app.use(bodyParser.json())
-app.use(authRoutes)
-app.use(patientRoutes)
-app.use(zielenGameRoutes)
-app.use(zielenTrainingRoutes)
-app.use(tippenGameRoutes)
-app.use(gewindeDataRoutes)
-app.use(turmeDataRoutes)
-app.use(nachfahrenDataRoutes)
-app.use(klotzeDataRoutes)
-app.use(umdrehenDataRoutes)
-
+app.use(authRoutes);
+app.use(patientRoutes);
+app.use(zielenGameRoutes);
+app.use(zielenTrainingRoutes);
+app.use(tippenGameRoutes);
+app.use(gewindeDataRoutes);
+app.use(turmeDataRoutes);
+app.use(nachfahrenDataRoutes);
+app.use(klotzeDataRoutes);
+app.use(umdrehenDataRoutes);
 
 mongoose.connect(mogoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.connection.on('connected', () => {
-    console.log("Connected")
-})
+  console.log("Connected");
+});
+
 mongoose.connection.on('error', (err) => {
-    console.log("Error", err)
-})
+  console.log("Error", err);
+});
 
 app.get('/', requireToken, (req, res) => {
-    res.send({ email: req.user.email, doctorId: req.user.doctorId })
+  res.send({ email: req.user.email, doctorId: req.user.doctorId });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
 })
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
